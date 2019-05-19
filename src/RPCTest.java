c8278d83d017ac8db8dffdc3939eebef29fd33e4
Johnscy <847841292@qq.com>
2019-05-19 20:30:23 +0800

    RPC的简单实现

--- src/RPCTest.java
+++ src/RPCTest.java
@@ -4,6 +4,6 @@ import java.net.InetSocketAddress;
 public class RPCTest {
     public static void main(String[] args) throws IOException {
         MyHelloService service = Client.getRemoteProxyObj(MyHelloService.class,new InetSocketAddress(8080));
-        System.out.println(service.sayHi("zhebang","hahaha"));
+        System.out.println(service.sayHi("zhebang","hahahaha"));
     }
 }
